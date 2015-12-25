394c94a274af85516f3c821646c3d88b99729a1b
sessionliang <liangjian@siteserver.cn>
2015-12-25 18:54:14 +0800

    添加channelController

---
+++ module/cms/CMS.Core/Common/PageUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Common
+{
+    public static class PageUtils
+    {
+        public static class CMSAdmin
+        {
+            public const string ADMIN_Url = "~/App/Main/views/";
+        }
+
+        public static string GetCMSAdminUrl(string relatedUrl)
+        {
+            if (string.IsNullOrEmpty(relatedUrl))
+                relatedUrl = "home/index";
+            relatedUrl = relatedUrl.Trim("/".ToCharArray());
+            return Path.Combine(CMSAdmin.ADMIN_Url, relatedUrl);
+        }
+    }
+}
---
+++ module/cms/CMS.Web/Controllers/ChannelController.cs
@@ -0,0 +1,31 @@
+﻿using CMS.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS.Web.Controllers
+{
+    public class ChannelController : Controller
+    {
+
+        /// <summary>
+        /// 栏目列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View(PageUtils.GetCMSAdminUrl("channel/channels.cshtml"));
+        }
+
+        /// <summary>
+        /// 栏目列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Channels()
+        {
+            return Index();
+        }
+    }
+}
